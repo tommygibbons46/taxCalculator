33f1b5d024b9c6679934ef8f683f8d6e631535c5
TommyGibbons46 <Tommygibbons46@gmail.com>
2015-03-05 00:30:37 -0500

    Initial Commit

---
+++ taxCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  taxCalculator
+//
+//  Created by Tommy Gibbons on 3/5/15.
+//  Copyright (c) 2015 Tommy Gibbons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
